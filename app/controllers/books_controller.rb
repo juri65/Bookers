9cfe392fa7b82c22a1c23288610c39d90d1d927f
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-07-14 01:04:55 +0000

    [add-9]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     if book.save
-     redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
     else
      render :index
     end 
